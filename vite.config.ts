0ab699a951c773a6c6c7ce8ff336cb7b11bcd3a9
afrizal074 <afrizalfauzi2003@gmail.com>
2025-11-05 13:25:22 +0700

    fix: konfigurasi base, router, dan folder actions

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // TAMBAHKAN BARIS INI
   base: "/myfirst_porto/", 
-  // --------------------
+  
 
   server: {
     host: "::",
